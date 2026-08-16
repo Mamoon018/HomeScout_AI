1928a8a44829af5e6e129b543facacdc3f9f89f4
Mamoon018 <mamonhaider11@gmail.com>
2026-08-16 13:31:54 +0200

    Deleted Vite Javascript Project

--- frontend/homescout_react/src/pages/info.md
+++
@@ -1,2 +0,0 @@
-
-This Page folder contains final Pages content and layout both that needs to be rendered by the application.
--- frontend/homescout_react/src/test/setup.js
+++
@@ -1,9 +0,0 @@
-import { afterEach } from "vitest"
-import { cleanup } from "@testing-library/react"
-import { toast } from "sonner"
-import "@testing-library/jest-dom/vitest"
-
-afterEach(() => {
-  cleanup()
-  toast.dismiss()
-})
